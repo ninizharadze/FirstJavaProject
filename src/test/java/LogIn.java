415954bd1d9087a614180600d56f956a7a4853c5
ninizharadze <nijaradze.nini@gmail.com>
2024-11-08 19:31:28 +0400

    Environment

--- src/test/java/LogIn.java
+++ src/test/java/LogIn.java
@@ -2,24 +2,48 @@ import com.codeborne.selenide.Selenide;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.Selenide.*;
+import static junit.framework.TestCase.assertEquals;
 
 public class LogIn {
 
     @Test
     public void LogInInvalid(){
         WebDriverManager.chromedriver().setup();
-        Selenide.open("https://dev.app.suada.com/");
+        WebDriver driver = new ChromeDriver();
+        Selenide.open("https://dev.admin.suada.com/login");
         sleep(1000);
 
-        $(byAttribute("type", "text")).setValue("nini+17@suada.com");
-        $(byAttribute("type", "password")).setValue("Qwertyuiop1$");
-        $(byText("Sign in")).click();
+        $(byAttribute("type", "text" )).setValue("super+1@gmail.com");
+        $(byAttribute("type", "password")).setValue("Qwerty1$");
+        $(byAttribute("type", "submit")).click();
+        sleep(2000);
 
-        Assert.assertEquals("Invalid credentials", $(byXpath("/html/body/div/section/div[2]/div/form/div[1]/p")).getText());
+//        String expectedUrl = "data:,";
+//        assertEquals(expectedUrl, driver.getCurrentUrl());
+
+        $(byText("Users")).click();
+        $(byText("Super Admins")).click();
+        $(byXpath("//button[contains(text(), 'Add Super Admin')]")).click();
+        $(byAttribute("name", "firstName")).setValue("New");
+        $(byAttribute("name", "lastName")).setValue("Super Admin");
+        $(byAttribute("name", "email")).setValue("new+superadmin@suada.com");
+        $(byXpath("//button[contains(text(), 'Save')]")).click();
+//         Check if an element with the exact text "New Super Admin has been added" is visible
+        $("#notistack-snackbar").shouldHave(text("New Super Admin has been added"));
+
+        $(".sc-bgykYu.iFQYHR.user_delete_icon").click();
+        $(byXpath("//button[contains(text(), 'Yes, Delete!')]")).click();
+        $("#notistack-snackbar").shouldHave(text("Super Admin has been deleted"));
+
     }
 }
